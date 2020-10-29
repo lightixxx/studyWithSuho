99fd89571bcd9e39c10e91530fdc9b3636545f69
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-10-29 21:18:59 +0900

    스터디 복습

--- day-4/app.js
+++ day-4/app.js
@@ -21,6 +21,10 @@ function min(numbers) {
   return minNumber;
 }
 
+const numbers = [0, -50, 444, 1, 23, 14, 5, 6, 234, 99];
+//console.log(max(numbers));
+//console.log(min(numbers));
+
 
 // sum 함수
 function sum(numbers) {
@@ -44,5 +48,34 @@ function countIf(arr, target) {
   return result;
 }
 
+const genders = ["남", "여", "여", "여", "남", "여", "남", "여"];
+// console.log(countIf(genders, "남")); // 3
+// console.log(countIf(genders, "여")); // 5
+
+
 // getDayOfWeek 함수
 // 조건문(식) 없이 구현
+// function getDayOfWeek(dayNum) {
+//   const dayOfWeek = ['Sunday', 'Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//   return dayOfWeek[dayNum];
+// }
+
+
+// getDayOfWeek 함수 리팩토링
+// 배열과 객체를 이용해서 ex) getDayOfWeek(0, ‘KR’), getDayOfWeek(1, ‘US’)
+function getDayOfWeek(day, code) {
+  const week = {
+    US : ['Sunday', 'Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    KR : ['일', '월', '화', '수' ,'목', '금', '토']
+  }
+
+  return week[code][day];
+}
+
+
+// jQuery의 $ selector 함수
+function $(selector) {
+  const target = document.querySelector(selector);
+}
+
