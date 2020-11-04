f7718a4e5d71f48753d44b91ee50159b5724d91c
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2020-11-04 23:37:23 +0900

    day-5 복습

--- day-5/app.js
+++ day-5/app.js
@@ -1,1 +1,53 @@
-// toggle 버튼 구현하기
+function Library(list) {
+  return {
+    find(name) {
+      const result = list.find(elem => name === elem);
+      return result === undefined ? 'Error' : result;
+    },
+    add(name) {
+      list.push(name);
+      return list;
+    },
+    list() {
+      return list;
+    },
+    delete(name) {
+      list.splice(list.indexOf(name), 1);
+      return list;
+    }
+  }
+}
+
+const bookList = ['자바스크립트완전정복', '파이썬', 'java의 정석'];
+const bookStore = new Library(bookList);
+// bookStore.find('파이썬') // 파이썬 (없다면 에러)
+// bookStore.add('HTML정복');
+// bookStore.list(); //bookList를 출력
+// bookStore.delete('파이썬'); // 파이썬 제거
+
+
+const person = {
+  name : '홍길동',
+  age : 25,
+  weight : 80,
+  diet : function(exercise){
+    // TODO :: 운동을 하는것, 나의 2키로씩 살이 빠진다.
+    if(exercise === 'run') return this.weight = this.weight - 2;
+    if(exercise === 'walk') return this.weight = this.weight - 1;
+    if(exercise === 'swim') return this.weight = this.weight - 4;
+    if(exercise === 'marathon') return this.weight = this.weight - 5;
+    return this.weight;
+  },
+  eat : function(food){
+    // TODO :: 먹는것, 나의 5키로씩 살이 찐다.
+    // return this.weight + 5;
+    if(food === 'hamburger') return this.weight = this.weight + 2;
+    if(food === 'pizza') return this.weight = this.weight + 3;
+    return this.weight;
+  }
+}
+
+// person.name;
+// person.weight;
+// person.diet('run');
+// person.eat()
--- day-6/app.js
+++ day-6/app.js
@@ -1,15 +1,54 @@
+// numbers 값을 이용해 square에 넓이 값 저장하기 
 const numbers = [2, 4, 8, 16, 32, 64];
 
-function getSquare(data) {
+// function getSquare(data) {
+//   const result = [];
+
+//   for(let i = 0; i < data.length; i++) {
+//     result.push(data[i] * data[i]);
+//   }
+
+//   return result;
+// }
+
+// const square = getSquare(numbers);
+
+
+// alphabet을 모두 대문자로 변환 후 upper에 저장하기
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+// function getUpper(str) {
+//   const result = [];
+
+//   for(let i = 0; i < str.length; i++) {
+//     result.push(str[i].toUpperCase());
+//   }
+
+//   return result;
+// }
+
+// const upper = getUpper(alphabet);
+
+
+// map 함수를 만들기
+function map(list, cb) {
   const result = [];
-
-  for(let i = 0; i < data.length; i++) {
-    result.push(data[i] * data[i]);
+  
+  for(let i = 0; i < list.length; i++) {
+    // result.push(cb(list[i]));
+    const d = cb(list[i]);
+    result.push(d);
   }
-
   return result;
 }
 
-const square = getSquare(numbers);
+const upper = map(alphabet, function (v) {
+  return v.toUpperCase();
+});
 
-console.log(square); // [[4,16,64,256,1024,4096]]
+const square = map(numbers, function (v) {
+  return v * v;
+});
+
+console.log(upper);
+console.log(square);
